45f7fca079ec54d3350c9c5cb11cf12fa194f241
Lucas Gonzalez <lucasgonzalez.dev@gmail.com>
2020-10-21 18:09:54 -0300

    added custom app propTypes & css globals

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import propTypes from 'prop-types'
+import '../node_modules/normalize.css/normalize.css'
+import '../static/styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: propTypes.func.isRequired,
+  pageProps: propTypes.object.isRequired
 }
 
 export default MyApp
