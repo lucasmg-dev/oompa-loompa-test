e211d60920866551ffedc69001e14ad2eddaa48b
Lucas Gonzalez <lucasgonzalez.dev@gmail.com>
2020-10-21 18:16:18 -0300

    update header

--- components/header/index.js
+++ components/header/index.js
@@ -1,9 +1,12 @@
+import style from './styles.module.css'
+
 import logo from '../../static/images/logo-umpa-loompa.png'
 
 export const Header = () => (
-  <header>
-    <nav>
-      <img src={logo} />
+  <header className={style.header}>
+    <nav className={`wrapper ${style.wrapper}`}>
+      <img src={logo} className={style.logo} />
+      <h1 className={style.title}>Oompa Loompa&apos;s Crew</h1>
     </nav>
   </header>
 )
