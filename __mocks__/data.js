34cbf22a5769361ee7df824e1e4490c730bed17a
Lucas Gonzalez <lucasgonzalez.dev@gmail.com>
2020-10-24 20:18:22 -0300

    addded mock oompa loompa results

---
+++ __mocks__/data.js
@@ -0,0 +1,42 @@
+module.exports = {
+  current: 1,
+  total: 20,
+  results: [
+    {
+      first_name: 'Marcy',
+      last_name: 'Karadzas',
+      favorite: {
+        color: 'red',
+        food: 'Chocolat'
+      },
+      random_string: '',
+      song: '',
+      gender: 'F',
+      image: 'https://s3.eu-central-1.amazonaws.com/napptilus/level-test/1.jpg',
+      profession: 'Developer',
+      email: 'mkaradzas1@visualengin.com',
+      age: 21,
+      country: 'Loompalandia',
+      height: 99,
+      id: 1
+    },
+    {
+      first_name: 'Evangelia',
+      last_name: 'Cowin',
+      favorite: {
+        color: 'red',
+        food: 'cocoa nuts'
+      },
+      random_string: '',
+      song: '',
+      gender: 'M',
+      image: 'https://s3.eu-central-1.amazonaws.com/napptilus/level-test/2.jpg',
+      profession: 'Metalworker',
+      email: 'ecowin2@visualengin.com',
+      age: 22,
+      country: 'Loompalandia',
+      height: 96,
+      id: 2
+    }
+  ]
+}
