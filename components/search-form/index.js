614668e92fb07af1c9851a7d6e1c89740fb2e4fd
Lucas Gonzalez <lucasgonzalez.dev@gmail.com>
2020-10-25 20:51:36 -0300

    update search form

--- components/search-form/index.js
+++ components/search-form/index.js
@@ -5,20 +5,22 @@ import searchIcon from '../../static/images/ic_search.png'
 
 export const SearchForm = ({ term, handleFilter }) => {
   return (
-    <div className='wrapper'>
-      <form className={styles.searchForm}>
-        <div className={styles.searchFormGroup}>
-          <input
-            type='text'
-            value={term}
-            onKeyUp={handleFilter}
-            placeholder='Search'
-            className={styles.searchInput} />
-          <div className={styles.searchIcon}>
-            <img src={searchIcon} width='15' />
+    <div className={styles.searchNav}>
+      <div className='wrapper'>
+        <form className={styles.searchForm}>
+          <div className={styles.searchFormGroup}>
+            <input
+              type='text'
+              value={term}
+              placeholder='Search'
+              className={styles.searchInput}
+              onKeyUp={(ev) => handleFilter(ev.target.value)}/>
+            <div className={styles.searchIcon}>
+              <img src={searchIcon} width='15' />
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   )
 }
--- components/search-form/index.test.js
+++ components/search-form/index.test.js
@@ -1,9 +1,18 @@
 import '@testing-library/jest-dom'
+import { fireEvent } from '@testing-library/dom'
 
 import { SearchForm } from './index'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+
+const onKeyUp = jest.fn()
 
 it('renders correctly', () => {
-  const { asFragment } = render(<SearchForm handleFilter={() => {}}/>)
+  const { asFragment } = render(<SearchForm handleFilter={onKeyUp} />)
   expect(asFragment()).toMatchSnapshot()
+})
+
+it('call filter handler', () => {
+  render(<SearchForm handleFilter={onKeyUp} />)
+  fireEvent.keyUp(screen.getByPlaceholderText('Search'))
+  expect(onKeyUp).toHaveBeenCalledTimes(1)
 })
