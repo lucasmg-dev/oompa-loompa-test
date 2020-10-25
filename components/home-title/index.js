78c7e6544c37d685a61097b96881a7b9a098047d
Lucas Gonzalez <lucasgonzalez.dev@gmail.com>
2020-10-25 19:42:35 -0300

    update homeTitle

--- components/home-title/index.js
+++ components/home-title/index.js
@@ -1,8 +1,8 @@
 import styles from './styles.module.css'
 
 export const HomeTitle = () => (
-  <>
+  <div className={styles.homeTitle}>
     <h1 className={styles.h1}>Find your Oompa Loompa</h1>
     <h2 className={styles.h2}>There are more than 100k</h2>
-  </>
+  </div>
 )
