08d3937b0d28373b17c5344a7f7fc8ec0e52fb58
Lucas Gonzalez <lucasgonzalez.dev@gmail.com>
2020-10-26 02:52:48 -0300

    update mock

--- __mocks__/data.js
+++ __mocks__/data.js
@@ -18,7 +18,8 @@ module.exports = {
       age: 21,
       country: 'Loompalandia',
       height: 99,
-      id: 1
+      id: 1,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
     },
     {
       first_name: 'Evangelia',
@@ -36,7 +37,8 @@ module.exports = {
       age: 22,
       country: 'Loompalandia',
       height: 96,
-      id: 2
+      id: 2,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
     }
   ]
 }
