cfc99465ab7d72df0da4636bccf557659375fa1d
Lucas Gonzalez <lucasgonzalez.dev@gmail.com>
2020-10-26 02:51:30 -0300

    added link home:

--- components/header/index.js
+++ components/header/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import style from './styles.module.css'
 
 import logo from '../../static/images/logo-umpa-loompa.png'
@@ -5,8 +6,12 @@ import logo from '../../static/images/logo-umpa-loompa.png'
 export const Header = () => (
   <header className={style.header}>
     <nav className={`wrapper ${style.wrapper}`}>
-      <img src={logo} className={style.logo} />
-      <span className={style.title}>Oompa Loompa&apos;s Crew</span>
+      <Link href='/'>
+        <a>
+          <img src={logo} className={style.logo} />
+          <span className={style.title}>Oompa Loompa&apos;s Crew</span>
+        </a>
+      </Link>
     </nav>
   </header>
 )
